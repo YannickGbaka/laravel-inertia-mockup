917884266b860278d27833ef56f3992a68fd596f
Gosse Yannick Cyriaque GB <gosseyannick553@gmail.com>
2024-08-21 23:50:02 +0000

    feat: progress indicator

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    sleep(2);
     $appName = env('APP_NAME');
     return Inertia::render('Home', compact('appName'));
 });
